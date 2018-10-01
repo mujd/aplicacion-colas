ae3d4c6a91cf2442b7429cb75d684216ef4c12ec
mujd <mujd19@gmail.com>
2018-10-01 03:13:27 -0300

    Primer commit -Producción

---
+++ public/js/socket.nuevo-ticket.js
@@ -0,0 +1,23 @@
+// Comando para establecer la conexión
+
+var socket = io();
+var label = $('#lblNuevoTicket');
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('desconectado del servidor');
+});
+
+socket.on('estadoActual', function(resp) {
+    console.log(resp);
+    label.text(resp.actual);
+});
+
+$('button').on('click', function() {
+    socket.emit('siguienteTicket', null, function(siguienteTicket) {
+        label.text(siguienteTicket);
+    });
+});
